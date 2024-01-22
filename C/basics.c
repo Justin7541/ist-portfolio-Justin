24832afcfcb01ed286f366efb21e22d5b381c909
Justin7541 <2026moreno.justin@fcboe.org>
2024-01-22 14:48:49 -0500

    finished basics code

---
+++ C/basics.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+ int main ()
+{
+	printf ("This is my c basics file\n" );
+
+	int num = 3;
+	printf ("Type any number: ");
+	scanf("%i", &num);
+	printf("Your number was %i\n", num);
+	return 0;
+} 
